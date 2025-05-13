31544b70c0a8e19f614ac1cd1ec4acaf8114695e
emersoneustaquio <eustaquioemerson2@gmil.com>
2025-05-13 17:12:12 -0300

    first commit

---
+++ src/test/java/TesteUser.java
@@ -0,0 +1,35 @@
+
+// 0 -Nome do pacote
+
+// 1- Biblioteca
+
+//2- Classe
+
+import org.junit.jupiter.api.Test;
+
+public class TesteUser {
+    //2.1 atributos
+    static String ct = "application/json"; // content-type
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+    //2.2 funções e metodos
+    //2.2.1 funcções e metrodos comuns / uteis
+    //2.2.2 métodos de teste
+
+    //Função de leitura de Json
+    public static String lerArquivoJson (String arquivoJson){
+        //To Do: Completar a leitura do arquivo
+        //return new String(Files)
+    }
+
+    @Test
+    public void testPostPet (){
+        // carregar os dados do arquivo json do usuario pet
+        // To Do : Criar o metodo Pos
+        //String jsonBory = 
+
+
+    }
+
+    
+}
