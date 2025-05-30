87e29723fed8a32310d076a38d70ede94b6787fa
emersoneustaquio <eustaquioemerson2@gmil.com>
2025-05-30 10:16:26 -0300

    Criado o teste para Post Pet

--- src/test/java/TestePet.java
+++ src/test/java/TestePet.java
@@ -4,21 +4,25 @@
 // 1- Biblioteca
 
 //2- Classe
-import io.restassured.response.Response;  //Classe Resposta do Rest-assured
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-  
 
-import static io.restassured.RestAssured.given; // função given da classe Rest Assured
 import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 
 public class TestePet {
-    //2.1 atributos
+    //2.1 atributos 
+        
+
     static String ct = "application/json"; // content-type
     static String uriPet = "https://petstore.swagger.io/v2/pet"; //URL base
+    
 
     //2.2 funções e metodos
     //2.2.1 funcções e metrodos comuns / uteis
@@ -32,34 +36,39 @@ public class TestePet {
         return new String(Files.readAllBytes(Paths.get(arquivoJson))) ;
     }
 
-    @Test
-    public void testPostPet () throws IOException{
-        // carregar os dados do arquivo json do usuario pet
-       String jsonBory = lerArquivoJson("src/test/resources/json/pet1.json"); // carregar arquivo json
+@Test
+public void testPostPet () throws IOException{
+    // Ignorando a verificação de SSL usando RestAssured
+    io.restassured.RestAssured.useRelaxedHTTPSValidation();
 
-       long petId = 170621001;  // codigo esperado do pet
+    // carregar os dados do arquivo json do usuario pet
+    String jsonBory = lerArquivoJson("src/test/resources/json/pet1.json"); // carregar arquivo json
+
+    int petId = 170621001;  // codigo esperado do pet
+    
+    // cl eça o teste via REST - ASSURED
+    Response response = 
+        given()                     //Dado que
+            .contentType(ct)           // Tipo do conteúdo é ct (aplication json)
+            .log().all()               // mostre tudo na ida      
+            .body(jsonBory)            // envie o corpo da requisição
+
+        .when()
+            .post(uriPet)           // chamando o endpoint fazendo post
+            
+        .then()                      //Então
+            .log().all()              //mostre tudo na volta
+            .statusCode(200)  // verifica se status code é 200
+            .body ("name", is("doggie"))  //verifica variavel name
+            .body("id",is(petId) )
+            .body("category.name",is("cachorro"))
+            .body("tags[0].name",is("vacinado"))
+            .extract().response(); //fim do given
         
-      // cl eça o teste via REST - ASSURED
+    System.out.println("Status Code: " + response.getStatusCode());  //Exibir status cide da resposta
+    System.out.println("Response Body: " + response.getBody().asString()); // Exibir corpo da resposta
 
-    given()                     //Dado que
-      .contentType(ct)           // Tipo do conteúdo é ct (aplication json)
-      .log().all()               // mostre tudo na ida      
-      .body(jsonBory)            // envie o corpo da requisição
-
-    .when()
-      .post(uriPet)           // chamando o endpoint fazendo post
-      
-    .then()                      //Então
-      .log().all()              //mostre tudo na volta
-      .statusCode(200)  // verifica se status code é 200
-      .body ("name", is("doggie"))  //verifica variavel name
-      .body("id",is(petId) )
-      .body("category.name",is("cachorro"))
-      .body("Tags[0].name",is("vacinado"))
-      ;
-
-
-    }
+}
 
     
 }
